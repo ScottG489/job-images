d0a1f69d4c3d67f268037a8f7d1cf6e4bf4019d9
Scott Giminiani <scottg489@gmail.com>
2020-10-20 16:24:42 -0400

    Return repo name and url along with badge
    - Move source code into src/
    - Introduce tsconfig.json

--- gh-repo-build-status/src/index.ts
+++ gh-repo-build-status/src/index.ts
@@ -1,6 +1,7 @@
 import {Endpoints} from "@octokit/types";
 import {Octokit} from "@octokit/rest";
 import {readFileSync} from "fs";
+import {RepoBadgeInfo} from "./types";
 
 const secretsFile = "/run/build/secrets/secrets";
 const secrets = JSON.parse(readFileSync(secretsFile, 'utf8'));
@@ -17,37 +18,44 @@ type ListUserReposResponse = Endpoints["GET /user/repos"]["response"];
 type ListUserRepoActionsParameters = Endpoints["GET /repos/:owner/:repo/actions/workflows"]["parameters"];
 type ListUserRepoActionsResponse = Endpoints["GET /repos/:owner/:repo/actions/workflows"]["response"];
 
-async function getRepoNames() {
+// TODO: This is a hack because I don't know how to get the full data type for data within the response
+interface SimplifiedUserRepoData {
+    name: string;
+    html_url: string;
+}
+
+async function getSimpleRepoData(): Promise<SimplifiedUserRepoData[]> {
     let userReposParams: ListUserReposParameters = {
         type: "owner",
         sort: "updated"
     }
     const userRepos = await octokit.repos.listForAuthenticatedUser(userReposParams)
-
-    return userRepos.data.map(repo => {
-        return repo.name
-    })
+    return userRepos.data
 }
 
-async function getWorkflowBadgeUrl(repoName: string) {
+async function getRepoBadgeInfo(simpleRepoData: SimplifiedUserRepoData): Promise<RepoBadgeInfo[]> {
     let userRepoActionParams: ListUserRepoActionsParameters = {
         owner: username,
-        repo: repoName
+        repo: simpleRepoData.name
     }
     const repoWorkflows = await octokit.actions.listRepoWorkflows(userRepoActionParams)
     return repoWorkflows.data.workflows
         .map(workflow => {
-            return workflow.badge_url
+            return {
+                repoName: simpleRepoData.name,
+                repoUrl: simpleRepoData.html_url,
+                badgeUrl: workflow.badge_url
+            }
         })
 }
 
 (async () => {
-    const repoNames = await getRepoNames();
-    const badgeUrls = (await Promise.all<string[]>(repoNames.map(getWorkflowBadgeUrl)))
+    const repoNames: SimplifiedUserRepoData[] = await getSimpleRepoData();
+    const badgeUrls = (await Promise.all<RepoBadgeInfo[]>(repoNames.map(getRepoBadgeInfo)))
         .filter(workflowBadges => {
             return Array.isArray(workflowBadges) && workflowBadges.length
-        }).map(f => {
-            return f[0]
+        }).map(workflowBadges => {
+            return workflowBadges[0]
         })
 
     console.log(JSON.stringify(badgeUrls, null, 2));
---
+++ gh-repo-build-status/src/types/index.ts
@@ -0,0 +1,5 @@
+export interface RepoBadgeInfo {
+  repoName: string;
+  repoUrl: string;
+  badgeUrl: string;
+}
