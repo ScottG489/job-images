0c2002fa1591eff77a4ea8910e156ad07c849714
Scott Giminiani <scottg489@gmail.com>
2020-11-17 23:51:50 -0500

    Create minimal echo image
    Simply echos back arguments. Generally useful but it's being created at
    this moment for testing other projects.

--- build.sh
+++ build.sh
@@ -47,3 +47,11 @@ declare -r DOCKER_CONFIG_CONTENTS_BASE64=$2
 ./mint-api-query/build.sh \
   "$ID_RSA_CONTENTS_BASE64" \
   "$DOCKER_CONFIG_CONTENTS_BASE64"
+
+
+./echo/build.sh \
+  "$ID_RSA_CONTENTS_BASE64" \
+  "$DOCKER_CONFIG_CONTENTS_BASE64" \
+  'git@github.com:ScottG489/job-images.git' \
+  'echo' \
+  'scottg489/echo-job:latest'
