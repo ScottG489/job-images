2f38444f2d3d4fffa1448b10ec9f9748020a769e
Scott Giminiani <scottg489@gmail.com>
2020-11-23 01:12:37 -0500

    Build on new conjob server

--- gh-repo-build-status/build.sh
+++ gh-repo-build-status/build.sh
@@ -1,6 +1,6 @@
 curl -v -sS -w '%{http_code}' \
   --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'"}' \
-  'http://api.simple-ci.com/build?image=scottg489/gh-repo-build-status-job-build:latest' \
+  'http://api.conjob.io/job/run?image=scottg489/gh-repo-build-status-job-build:latest' \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
