65807e8702a615c94d88a1b7ed5a53c9b2957570
Scott Giminiani <scottg489@gmail.com>
2021-01-14 23:06:15 -0500

    Use new job runner for build
    simple-ci.com is where the legacy job runner is hosted. conjob.io is the
    new server.

--- google-takeout-export/build.sh
+++ google-takeout-export/build.sh
@@ -1,6 +1,19 @@
-curl -v -sS -w '%{http_code}' \
-  --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'"}' \
-  'http://api.simple-ci.com/build?image=scottg489/google-takeout-export-job-build:latest' \
+#!/usr/bin/env bash
+
+readonly IMAGE_NAME='scottg489/google-takeout-export-job-build:latest'
+readonly ID_RSA=$1
+readonly DOCKER_CONFIG=$2
+
+read -r -d '' JSON_BODY <<- EOM
+  {
+  "ID_RSA": "$ID_RSA",
+  "DOCKER_CONFIG": "$DOCKER_CONFIG",
+  }
+EOM
+
+curl -v -sS -w '\n%{http_code}' \
+  --data-binary "$JSON_BODY" \
+  "http://api.conjob.io/job/run?image=$IMAGE_NAME" \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
