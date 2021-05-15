8297f6027fc4b671e0ab39da044abd0995e030f6
Scott Giminiani <scottg489@gmail.com>
2021-05-15 08:13:33 -0700

    Add new test-support parameter of file name to output contents of
    This is being added so that we can test the functionality of secrets
    in conjob.

--- test-support/run.sh
+++ test-support/run.sh
@@ -3,10 +3,13 @@ set -e
 
 SLEEP=$(echo -n $1 | cut -d '|' -f 1)
 OUTPUT=$(echo -n $1 | cut -d '|' -f 2)
-EXIT_CODE=$(echo -n $1 | cut -d '|' -f 3)
+FILE_NAME=$(echo -n $1 | cut -d '|' -f 3)
+EXIT_CODE=$(echo -n $1 | cut -d '|' -f 4)
 
 sleep "$SLEEP"
 
 echo -n "$OUTPUT"
+
+[ -n "$FILE_NAME" ] && cat "$FILE_NAME"
 
 exit "$EXIT_CODE"
