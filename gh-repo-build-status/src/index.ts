b2f38c59bcbb3622ac64dca83bd34dda6e840be9
Scott Giminiani <scottg489@gmail.com>
2025-12-14 19:29:20 -0500

    Report status from all builds, not just the latest
    Before we were just getting the status of the latest build. However, if
    we have multiple workflows this doesn't give an accurate representation
    of the repos build status.
    
    Now, we look at the status of all checks from every branch and if any
    are failing we consider the whole repo to be failing.
    
    - Since we used flat() we also needed to fix some compilation errors by
    upgrading the node version and including the tsconfig.

--- gh-repo-build-status/src/index.ts
+++ gh-repo-build-status/src/index.ts
@@ -1,7 +1,7 @@
 import {Endpoints} from "@octokit/types";
 import {Octokit} from "@octokit/rest";
 import {readFileSync} from "fs";
-import {RepoBadgeInfo, RepoBuildInfo} from "./types";
+import { RepoBuildInfo } from "./types";
 
 const secretsFile = "/run/build/secrets/secrets";
 const userSecrets = process.argv[2]
@@ -15,22 +15,11 @@ const octokit = new Octokit({
 
 type ListUserReposParameters = Endpoints["GET /user/repos"]["parameters"];
 
-type ListUserRepoActionsParameters = Endpoints["GET /repos/:owner/:repo/actions/workflows"]["parameters"];
-type ListWorkflowRunsParams = Endpoints["GET /repos/:owner/:repo/actions/runs"]["parameters"];
-
 // TODO: This is a hack because I don't know how to get the full data type for data within the response
 interface SimplifiedUserRepoData {
     name: string;
     html_url: string;
     fork: boolean;
-}
-
-interface SimplifiedRepoWorkflow {
-    badge_url: string;
-}
-
-interface SimplifiedWorkflowRun {
-    conclusion: string | null;
 }
 
 async function listSimpleRepoData(): Promise<SimplifiedUserRepoData[]> {
@@ -39,54 +28,27 @@ async function listSimpleRepoData(): Promise<SimplifiedUserRepoData[]> {
         sort: "updated",
         per_page: 100
     }
-    return await (await octokit.repos.listForAuthenticatedUser(userReposParams)).data
+    return (await octokit.repos.listForAuthenticatedUser(userReposParams)).data
 }
 
-async function listSimpleRepoWorkflows(simpleRepoData: SimplifiedUserRepoData): Promise<SimplifiedRepoWorkflow[]> {
-    let userRepoActionParams: ListUserRepoActionsParameters = {
+async function getBranches(repo: SimplifiedUserRepoData) {
+    return (await octokit.repos.listBranches({
         owner: username,
-        repo: simpleRepoData.name,
-        page: 1,
-        per_page: 1
-    }
-    return (await octokit.actions.listRepoWorkflows(userRepoActionParams)).data.workflows
+        repo: repo.name,
+    })).data;
 }
 
-async function listWorkflowRuns(badgeInfo: RepoBadgeInfo): Promise<SimplifiedWorkflowRun[]> {
-    let userRepoListWorkflowRunsParams: ListWorkflowRunsParams = {
+async function getBranchBuildStatus(repo: SimplifiedUserRepoData, branchName: string) {
+    return (await octokit.checks.listSuitesForRef({
         owner: username,
-        repo: badgeInfo.repoName,
-        page: 1,
-        per_page: 1
-    }
-    return (await octokit.actions.listWorkflowRunsForRepo(userRepoListWorkflowRunsParams)).data.workflow_runs
-}
-
-async function getRepoBadgeInfo(simpleRepoData: SimplifiedUserRepoData): Promise<RepoBadgeInfo[]> {
-    const repoWorkflows = await listSimpleRepoWorkflows(simpleRepoData)
-
-    return repoWorkflows
-        .map(workflow => {
-            return {
-                repoName: simpleRepoData.name,
-                repoUrl: simpleRepoData.html_url,
-                badgeUrl: workflow.badge_url
-            }
-        })
-}
-
-async function getRepoBuildInfo(badgeInfo: RepoBadgeInfo): Promise<RepoBuildInfo[]> {
-    const workflowRuns: SimplifiedWorkflowRun[] = await listWorkflowRuns(badgeInfo)
-
-    return workflowRuns
-        .map(workflowRun => {
-            return {
-                repoName: badgeInfo.repoName,
-                repoUrl: badgeInfo.repoUrl,
-                badgeUrl: badgeInfo.badgeUrl,
-                workflowRunConclusion: workflowRun.conclusion === null ? "" : workflowRun.conclusion
-            }
-        })
+        repo: repo.name,
+        ref: branchName
+    })).data.check_suites
+        .filter(checkSuite => {
+            return checkSuite.status === 'completed'
+        }).map(checkSuite => {
+            return checkSuite.conclusion
+        });
 }
 
 (async () => {
@@ -94,21 +56,25 @@ async function getRepoBuildInfo(badgeInfo: RepoBadgeInfo): Promise<RepoBuildInfo
         .filter(repo => {
             return !repo.fork;
         });
-    const badgeInfos: RepoBadgeInfo[] = (await Promise.all<RepoBadgeInfo[]>(simpleRepos.map(getRepoBadgeInfo)))
-        .filter(workflowBadges => {
-            return Array.isArray(workflowBadges) && workflowBadges.length
-        }).map(workflowBadges => {
-            return workflowBadges[0]
-        })
-    const buildInfos: RepoBuildInfo[] =
-        (await Promise.all<RepoBuildInfo[]>(badgeInfos.map(getRepoBuildInfo)))
-            .filter((workflowBuilds: RepoBuildInfo[]) => {
-                return Array.isArray(workflowBuilds) && workflowBuilds.length
-            }).map((workflowBuilds: RepoBuildInfo[]) => {
-            return workflowBuilds[0]
-        })
 
-    console.log(JSON.stringify(buildInfos, null, 2));
+    const repoBuildStatus = (await Promise.all(simpleRepos.map(async repo => {
+        const branches = (await getBranches(repo))
+            .map(branch => branch.name);
+
+        const branchResults = (await Promise.all(branches.map(async branchName => {
+            return (await getBranchBuildStatus(repo, branchName))
+        }))).flat();
+
+        if (Array.isArray(branchResults) && branchResults.length) {
+            return {
+                repoName: repo.name,
+                repoUrl: repo.html_url,
+                conclusion: branchResults.some(result => result === 'failure') ? 'failure' : 'success'
+            }
+        }
+    }))).filter(i => i !== undefined)
+
+    console.log(JSON.stringify(repoBuildStatus, null, 2));
 })().catch(e => {
     console.log("Fail")
     console.log(e);
--- gh-repo-build-status/src/types/index.ts
+++ gh-repo-build-status/src/types/index.ts
@@ -1,12 +1,5 @@
-export interface RepoBadgeInfo {
-  repoName: string;
-  repoUrl: string;
-  badgeUrl: string;
-}
-
 export interface RepoBuildInfo {
   repoName: string;
   repoUrl: string;
-  badgeUrl: string;
-  workflowRunConclusion: string;
+  conclusion: string;
 }
